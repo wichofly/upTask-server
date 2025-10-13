be39f50dcfd5f98eb851a26e8c4598e4e8eb9f9e
Mauricio Erazo <antonierazo13@hotmail.com>
2025-10-13 16:27:05 +0200

    feat: add colors package and update console log styling in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import server from './server';
+import colors from 'colors';
 
 const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
-  console.log(`REST API works on ${port}`);
+  console.log(colors.blue.bold(`REST API works on ${port}`));
 });
