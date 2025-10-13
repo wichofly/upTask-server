e01899db0ea8867179d7a582aab87e9fb913c5f4
Mauricio Erazo <antonierazo13@hotmail.com>
2025-10-13 23:52:24 +0200

    feat: implement ProjectController and projectRoutes for project management API

---
+++ src/controllers/ProjectController.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from 'express';
+
+export class ProjectController {
+  static getAllProjects = async (req: Request, res: Response) => {
+    res.send('Get all projects');
+  };
+}
