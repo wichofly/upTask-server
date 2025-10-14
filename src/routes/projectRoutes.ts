958df158f4d0064c9705846b637d5a350a5a3c9d
Mauricio Erazo <antonierazo13@hotmail.com>
2025-10-14 17:56:47 +0200

    feat: add express-validator for input validation in project creation

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,9 +1,21 @@
 import { Router } from 'express';
 import { ProjectController } from '../controllers/ProjectController';
+import { body } from 'express-validator';
+import { handleInputErrors } from '../middleware/validation';
 
 const router = Router();
 
-router.post('/', ProjectController.createProject);
+router.post(
+  '/',
+  body('projectName').notEmpty().withMessage('Project name is required'),
+  body('clientName').notEmpty().withMessage('Client name is required'),
+  body('description')
+    .notEmpty()
+    .withMessage('Description of the project is required'),
+
+  handleInputErrors,
+  ProjectController.createProject
+);
 
 router.get('/', ProjectController.getAllProjects);
 
