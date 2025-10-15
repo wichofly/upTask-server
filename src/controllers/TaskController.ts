a32a8119201eec27da4cf10597caf9bc296b3813
Mauricio Erazo <antonierazo13@hotmail.com>
2025-10-15 20:12:29 +0200

    feat: refactor task creation to use validated project from middleware

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -1,19 +1,14 @@
 import { Request, Response } from 'express';
-import Project from '../models/Project';
 import Task from '../models/Task';
 
 export class TaskController {
   static createTask = async (req: Request, res: Response) => {
-    const { projectId } = req.params;
-    const project = await Project.findById(projectId);
-    if (!project) return res.status(404).send({ error: 'Project not found' });
-
     try {
       const task = new Task(req.body);
-      task.project = project.id; // Associate task with the project
-      project.tasks.push(task.id); // Add task to the project's tasks array
+      task.project = req.project.id; // Associate task with the project
+      req.project.tasks.push(task.id); // Add task to the project's tasks array
       await task.save();
-      await project.save();
+      await req.project.save();
       res.send('Task created successfully');
     } catch (error) {
       console.log(error);
--- src/middleware/projects.ts
+++ src/middleware/projects.ts
@@ -1,5 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
-import Project from '../models/Project';
+import Project, { IProject } from '../models/Project';
+
+declare global {
+  namespace Express {
+    interface Request {
+      project: IProject;
+    }
+  }
+}
 
 export const validateProjectExists = async (
   req: Request,
@@ -10,6 +18,7 @@ export const validateProjectExists = async (
     const { projectId } = req.params;
     const project = await Project.findById(projectId);
     if (!project) return res.status(404).json({ message: 'Project not found' });
+    req.project = project; // Attach project to request object
     next();
   } catch (error) {
     res.status(500).json({ error: 'Server error' });
