cff11c8acde5117e5a0bde498d881ba0ca59c402
Mauricio Erazo <antonierazo13@hotmail.com>
2025-10-15 13:40:19 +0200

    feat: add task status management to ITask interface and TaskSchema

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,9 +1,20 @@
 import mongoose, { Schema, Document, Types } from 'mongoose';
 
+const taskStatus = {
+  PENDING: 'pending',
+  ON_HOLD: 'onHold',
+  IN_PROGRESS: 'inProgress',
+  UNDER_REVIEW: 'underReview',
+  COMPLETED: 'completed',
+} as const;
+
+export type TaskStatus = (typeof taskStatus)[keyof typeof taskStatus];
+
 export interface ITask extends Document {
   name: string;
   description: string;
   project: Types.ObjectId;
+  status: TaskStatus;
 }
 
 export const TaskSchema: Schema = new Schema(
@@ -11,6 +22,11 @@ export const TaskSchema: Schema = new Schema(
     name: { type: String, trim: true, required: true },
     description: { type: String, trim: true, required: true },
     project: { type: Types.ObjectId, ref: 'Project' },
+    status: {
+      type: String,
+      enum: Object.values(taskStatus),
+      default: taskStatus.PENDING,
+    },
   },
   { timestamps: true } // Automatically manage createdAt and updatedAt fields
 );
