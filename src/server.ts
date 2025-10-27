72096f008b98a122c6c2416155566469ffd08b2a
Mauricio Erazo <antonierazo13@hotmail.com>
2025-10-27 19:24:00 +0100

    feat: add CORS configuration and update server to use it

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import { corsConfig } from './config/cors';
 import { connectDB } from './config/db';
 import projectRoutes from './routes/projectRoutes';
 
@@ -8,6 +10,8 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+app.use(cors(corsConfig)); // CORS configuration
 
 app.use(express.json()); // Middleware to parse JSON
 
