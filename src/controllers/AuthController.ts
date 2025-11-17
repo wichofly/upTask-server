191c3f04ce79babe7b79d7307e6a15bcd89ac218
Mauricio Erazo <antonierazo13@hotmail.com>
2025-11-17 21:53:45 +0100

    feat: enhance account creation to save user data and send confirmation email prompt

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,9 +1,14 @@
 import { Request, Response } from 'express';
+import User from '../models/User';
 
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
     try {
-      res.send('Account created successfully');
+      const user = new User(req.body);
+      await user.save();
+      res.send(
+        'Account created successfully, check your email to confirm your account'
+      );
     } catch (error) {
       res.status(500).json({ error: 'Server error' });
     }
