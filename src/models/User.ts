9fdad5481c793a8fa9eb6e80d9c05307e95e85c5
Mauricio Erazo <antonierazo13@hotmail.com>
2025-11-17 20:23:43 +0100

    feat: create User model with schema and interface for user data

---
+++ src/models/User.ts
@@ -0,0 +1,18 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface IUser extends Document {
+  email: string;
+  password: string;
+  name: string;
+  confirmed: boolean;
+}
+
+export const UserSchema: Schema = new Schema({
+  email: { type: String, required: true, lowercase: true, unique: true },
+  password: { type: String, required: true },
+  name: { type: String, required: true },
+  confirmed: { type: Boolean, default: false },
+});
+
+const User = mongoose.model<IUser>('User', UserSchema);
+export default User;
