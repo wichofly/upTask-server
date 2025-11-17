b350430e62643e1c42a897395af32e6629786dca
Mauricio Erazo <antonierazo13@hotmail.com>
2025-11-17 22:09:01 +0100

    feat: add bcrypt for password hashing during account creation

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,10 +1,16 @@
 import { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
 import User from '../models/User';
 
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
     try {
       const user = new User(req.body);
+
+      // hash password
+      const salt = await bcrypt.genSalt(10)
+      user.password = await bcrypt.hash(req.body.password, salt)
+
       await user.save();
       res.send(
         'Account created successfully, check your email to confirm your account'
