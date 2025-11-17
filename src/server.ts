ccb6784c911ccb30d236137252397592de6285c6
Mauricio Erazo <antonierazo13@hotmail.com>
2025-11-17 21:09:58 +0100

    feat: add authentication routes for user authentication

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import morgan from 'morgan';
 import { corsConfig } from './config/cors';
 import { connectDB } from './config/db';
+import authRoutes from './routes/authRoutes';
 import projectRoutes from './routes/projectRoutes';
 
 dotenv.config();
@@ -19,6 +20,7 @@ app.use(morgan('dev')); // HTTP request logger
 app.use(express.json()); // Middleware to parse JSON
 
 // Routes
+app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
 
 export default app;
