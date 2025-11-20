e31054e9d5b75719a2bc8a4a7e362ef2873289aa
Mauricio Erazo <antonierazo13@hotmail.com>
2025-11-20 23:43:57 +0100

    fix: update confirmation email link to use environment variable for frontend URL

--- src/emails/AuthEmail.ts
+++ src/emails/AuthEmail.ts
@@ -14,7 +14,7 @@ export class AuthEmail {
       subject: 'Confirm your account',
       text: `UpTask - Confirm your account`,
       html: `<p>Please confirm your account <strong>${user.name}</strong> by clicking the following link:</p>
-             <a href="http://yourfrontend.com/confirm/">Confirm Account</a>
+             <a href="${process.env.FRONTEND_URL}/auth/confirm-account">Confirm Account</a>
              <p>Insert the code: <b>${user.token}</b></p>
              <p>This link will expire in 10 minutes.</p>
              `,
