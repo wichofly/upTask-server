7276ca1fa2c87d2442601d3054196996cb78754d
Mauricio Erazo <antonierazo13@hotmail.com>
2026-01-14 22:18:14 +0100

    refactor: Project model to include middleware for cascading deletes of tasks and notes

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,6 +1,7 @@
 import mongoose, { Schema, Document, Types, PopulatedDoc } from 'mongoose';
-import { ITask } from './Task';
+import Task, { ITask } from './Task';
 import { IUser } from './User';
+import Note from './Note';
 
 // Define for TypeScript
 export interface IProject extends Document {
@@ -36,6 +37,19 @@ const ProjectSchema: Schema = new Schema(
   },
   { timestamps: true } // Automatically manage createdAt and updatedAt fields
 );
+
+// Middleware
+ProjectSchema.pre('deleteOne', { document: true }, async function () {
+  const projectId = this._id;
+  if (!projectId) return;
+
+  const tasks = await Task.find({ project: projectId }); // deletes notes related to tasks in the project
+  for (const task of tasks) {
+    await Note.deleteMany({ task: task.id });
+  }
+
+  await Task.deleteMany({ project: projectId }); // deletes tasks related to the project
+});
 
 const Project = mongoose.model<IProject>('Project', ProjectSchema);
 export default Project;
